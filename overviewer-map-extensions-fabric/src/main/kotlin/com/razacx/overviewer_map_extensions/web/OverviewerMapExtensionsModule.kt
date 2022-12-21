a06d5cbf5f3a49630d02521f0a2c91e2e5df1260
Levi Vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2022-12-21 12:44:21 +0100

    Revert "Remove extension functions to see if it makes a difference"
    This reverts commit 620b0e8cba4e8ba620051b9561b490682d3f01ec.

--- overviewer-map-extensions-fabric/src/main/kotlin/com/razacx/overviewer_map_extensions/web/OverviewerMapExtensionsModule.kt
+++ overviewer-map-extensions-fabric/src/main/kotlin/com/razacx/overviewer_map_extensions/web/OverviewerMapExtensionsModule.kt
@@ -8,55 +8,63 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
+import io.reactivex.rxjava3.disposables.Disposable
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import java.util.concurrent.TimeUnit
 
 fun startWebServer() {
-    embeddedServer(Netty, port = 8080) {
-        install(WebSockets) {
-            contentConverter = KotlinxWebsocketSerializationConverter(Json { encodeDefaults = true })
-        }
-        routing {
-            webSocket("/ws") {
-                println("Opened websocket session")
-                val playerSubscription = players.throttleLast(100, TimeUnit.MILLISECONDS).subscribe {
-                    val playersMessage = PlayersMessage(it.map { player ->
-                        Player(
-                            player.uuidAsString,
-                            player.name.string,
-                            Position(player.pos.x, player.pos.y, player.pos.z),
-                            player.getWorld().registryKey.value.toString()
-                        )
-                    })
-                    launch {
-                        try {
-                            sendSerialized(playersMessage)
-                        } catch (e: Exception) {
-                            println(e)
-                        }
-                    }
-                }
-                val timeSubscription = timeOfDay.subscribe { timeOfDay ->
-                    val timeMessage = TimeMessage(timeOfDay)
-                    launch {
-                        try {
-                            sendSerialized(timeMessage)
-                        } catch (e: Exception) {
-                            println(e)
-                        }
-                    }
-                }
-
-                for (message in incoming) {
-                    // Keep web socket alive
-                }
-
-                println("Closed websocket session")
-                playerSubscription.dispose()
-                timeSubscription.dispose()
-            }
-        }
-    }.start(wait = false)
+    embeddedServer(Netty, port = 8080, module = Application::overviewerMapExtensionsModule).start(wait = false)
 }
 
+fun Application.overviewerMapExtensionsModule() {
+    install(WebSockets) {
+        contentConverter = KotlinxWebsocketSerializationConverter(Json { encodeDefaults = true })
+    }
+    routing {
+        webSocket("/ws") {
+            println("Opened websocket session")
+            val playerSubscription = doPlayerUpdates(100, TimeUnit.MILLISECONDS)
+            val timeSubscription = doTimeUpdates()
+
+            for (message in incoming) {
+                // Keep web socket alive
+            }
+
+            println("Closed websocket session")
+            playerSubscription.dispose()
+            timeSubscription.dispose()
+        }
+    }
+}
+
+private fun WebSocketServerSession.doTimeUpdates(): Disposable {
+    return timeOfDay.subscribe { timeOfDay ->
+        val timeMessage = TimeMessage(timeOfDay)
+        sendSerializedSafe(timeMessage)
+    }
+}
+
+private fun WebSocketServerSession.doPlayerUpdates(interval: Long, unit: TimeUnit): Disposable {
+    return players.throttleLast(interval, unit).subscribe {
+        val playersMessage = PlayersMessage(it.map { player ->
+            Player(
+                player.uuidAsString,
+                player.name.string,
+                Position(player.pos.x, player.pos.y, player.pos.z),
+                player.getWorld().registryKey.value.toString()
+            )
+        })
+        sendSerializedSafe(playersMessage)
+    }
+}
+
+private inline fun <reified T> WebSocketServerSession.sendSerializedSafe(data: T) {
+    launch {
+        try {
+            sendSerialized(data)
+        } catch (e: Exception) {
+            println(e)
+        }
+    }
+}
